86c6166aef7dfcaf6b58b77a78a058dcd70ac8e8
Carles <cpcastells@gmail.com>
2023-08-15 18:55:16 +0200

    Add conditional rendering of colors and user balance info

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import logo from "../../../public/images/splitify-logo.svg";
 import Operation from "../Operation/Operation";
-import Contact from "../Contact/Contact";
 
 const App = (): React.ReactElement => {
   return (
@@ -15,7 +14,6 @@ const App = (): React.ReactElement => {
       />
       <div className="h-screen bg-gradient-to-b from-white to-orange-200 flex flex-col justify-start items-center gap-5 px-5 pb-9">
         <Operation />
-        <Contact />
       </div>
     </>
   );
--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,18 +1,44 @@
 import React from "react";
+import { ContactData } from "../../types";
 
-const Contact = (): React.ReactElement => {
+interface ContactProps {
+  contact: ContactData;
+}
+
+const Contact = ({
+  contact: { name, image, balance },
+}: ContactProps): React.ReactElement => {
   return (
-    <article className="flex justify-between items-center w-full p-[14px] bg-[#f9f4ee] rounded-[10px] shadow-md">
+    <article
+      className={`flex justify-between items-center w-full p-[14px] {} ${
+        balance < 0
+          ? "bg-[#ffdbd7]"
+          : balance === 0
+          ? "bg-[#f9f4ee]"
+          : "bg-[#dbf2e3]"
+      } rounded-[10px] shadow-md`}
+    >
       <img
-        src="https://cdn.discordapp.com/attachments/1114204200885301331/1141028891218170047/foto.png"
-        alt="portrait of xxx"
+        src={image}
+        alt={`portrait of ${name}`}
         width={100}
         height={100}
         className="rounded-full"
       />
       <div className="flex flex-col justify-between items-end">
-        <h2 className="text-xl font-bold">XXX</h2>
-        <p className="py-5">You and XXX are even</p>
+        <h2 className="text-xl font-bold">{name}</h2>
+        {balance < 0 ? (
+          <p className="py-5 font-bold text-[#ce0f0f]">
+            You owe {name} {-balance}€
+          </p>
+        ) : balance === 0 ? (
+          <p className="py-5">You and {name} are even</p>
+        ) : (
+          <p className="py-5 font-bold text-[#199561]">
+            {name} owes you {balance}
+          </p>
+        )}
+
         <button className="w-[72px] h-[41px] bg-[#f8a23d] font-bold text-white rounded-[10px] shadow-md">
           Select
         </button>
