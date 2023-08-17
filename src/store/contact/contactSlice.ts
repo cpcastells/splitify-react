50f9f8b6d3b22c0beec0cdb008f3768452dd1ad9
Carles <cpcastells@gmail.com>
2023-08-17 19:04:04 +0200

    Create contactSlice with addContact reducer

---
+++ src/store/contact/contactSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { ContactData, ContactStateStructure } from "../../types";
+import { initialContacts } from "../../data/contacts";
+
+export const initialContactState: ContactStateStructure = {
+  friends: initialContacts,
+  friend: {
+    id: "",
+    name: "",
+    image: "",
+    balance: 0,
+  },
+};
+
+const contactSlice = createSlice({
+  name: "contact",
+  initialState: initialContactState,
+  reducers: {
+    addContact: (currentContactState, action: PayloadAction<ContactData>) => ({
+      ...currentContactState,
+      friends: [...currentContactState.friends, action.payload],
+    }),
+  },
+});
+
+export const { addContact: addContactsActionCreator } = contactSlice.actions;
+export const contactReducer = contactSlice.reducer;
