cffbbc3c20fcee7f0b7f3e3a04db019efa9bb125
Carles <cpcastells@gmail.com>
2023-08-17 20:57:40 +0200

    Add test for selectContact reducer

--- src/components/Contact/Contact.test.tsx
+++ src/components/Contact/Contact.test.tsx
@@ -1,7 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import Contact from "./Contact";
 import { contactMockList } from "../../mocks/mocks";
 import { vi } from "vitest";
+import renderWithProviders from "../../utils/testUtils";
 
 describe("Given an Contact component", () => {
   describe("When it receives the data of Johnny", () => {
@@ -10,7 +11,9 @@ describe("Given an Contact component", () => {
     test("Then it should show a heading with the name 'Johnny'", () => {
       const expectedText = /johnny/i;
 
-      render(<Contact contact={contactMockList[0]} onSelect={onSelect} />);
+      renderWithProviders(
+        <Contact contact={contactMockList[0]} onSelect={onSelect} />
+      );
       const text = screen.getByRole("heading", { name: expectedText });
 
       expect(text).toBeInTheDocument();
@@ -19,7 +22,9 @@ describe("Given an Contact component", () => {
     test("Then it should show Johnny's picture", () => {
       const expectedText = /portrait of johnny/i;
 
-      render(<Contact contact={contactMockList[0]} onSelect={onSelect} />);
+      renderWithProviders(
+        <Contact contact={contactMockList[0]} onSelect={onSelect} />
+      );
       const text = screen.getByAltText(expectedText);
 
       expect(text).toBeInTheDocument();
--- src/components/ContactList/ContactList.test.tsx
+++ src/components/ContactList/ContactList.test.tsx
@@ -1,7 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import ContactList from "./ContactList";
 import { contactMockList } from "../../mocks/mocks";
 import { vi } from "vitest";
+import renderWithProviders from "../../utils/testUtils";
 
 describe("Given an ContactList component", () => {
   describe("When it receives a list of Friends", () => {
@@ -11,7 +12,9 @@ describe("Given an ContactList component", () => {
     test("Then it should show a heading with the name 'Johnny'", () => {
       const expectedText = /johnny/i;
 
-      render(<ContactList contactList={friendsList} onSelect={onSelect} />);
+      renderWithProviders(
+        <ContactList contactList={friendsList} onSelect={onSelect} />
+      );
 
       const text = screen.getByRole("heading", { name: expectedText });
 
@@ -19,7 +22,9 @@ describe("Given an ContactList component", () => {
     });
 
     test("Then it should renders a list of 3 friends", () => {
-      render(<ContactList contactList={friendsList} onSelect={onSelect} />);
+      renderWithProviders(
+        <ContactList contactList={friendsList} onSelect={onSelect} />
+      );
 
       const friendElements = screen.getAllByRole("listitem");
 
