8dc74a7d07205b7e80056f48399578e2fa720710
Carles <cpcastells@gmail.com>
2023-08-17 20:29:06 +0200

    Add test for addContact reducer

--- src/components/ContactList/ContactList.test.tsx
+++ src/components/ContactList/ContactList.test.tsx
@@ -1,15 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import ContactList from "./ContactList";
 import { contactMockList } from "../../mocks/mocks";
+import { vi } from "vitest";
 
 describe("Given an ContactList component", () => {
   describe("When it receives a list of Friends", () => {
     const friendsList = contactMockList;
+    const onSelect = vi.fn();
 
     test("Then it should show a heading with the name 'Johnny'", () => {
       const expectedText = /johnny/i;
 
-      render(<ContactList contactList={friendsList} />);
+      render(<ContactList contactList={friendsList} onSelect={onSelect} />);
 
       const text = screen.getByRole("heading", { name: expectedText });
 
@@ -17,7 +19,7 @@ describe("Given an ContactList component", () => {
     });
 
     test("Then it should renders a list of 3 friends", () => {
-      render(<ContactList contactList={friendsList} />);
+      render(<ContactList contactList={friendsList} onSelect={onSelect} />);
 
       const friendElements = screen.getAllByRole("listitem");
 
