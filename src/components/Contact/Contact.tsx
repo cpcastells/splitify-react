1d1f729d0169db954e1a08a3425005e17abadc34
Carles <cpcastells@gmail.com>
2023-08-17 20:49:49 +0200

    Add contact data to store on clicking select

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,49 +1,55 @@
 import React from "react";
 import { ContactData } from "../../types";
+import { useAppDispatch } from "../../store";
+import { selectContactActionCreator } from "../../store/contact/contactSlice";
 
 interface ContactProps {
   contact: ContactData;
   onSelect: () => void;
 }
 
-const Contact = ({
-  contact: { name, image, balance },
-  onSelect,
-}: ContactProps): React.ReactElement => {
+const Contact = ({ contact, onSelect }: ContactProps): React.ReactElement => {
+  const dispatch = useAppDispatch();
+
+  const handleOnSelect = (): void => {
+    dispatch(selectContactActionCreator(contact));
+    onSelect();
+  };
+
   return (
     <article
       className={`flex justify-between items-center w-full p-[14px] {} ${
-        balance < 0
+        contact.balance < 0
           ? "bg-[#ffdbd7]"
-          : balance === 0
+          : contact.balance === 0
           ? "bg-[#f9f4ee]"
           : "bg-[#dbf2e3]"
       } rounded-[10px] shadow-md`}
     >
       <img
-        src={image}
-        alt={`portrait of ${name}`}
+        src={contact.image}
+        alt={`portrait of ${contact.name}`}
         width={120}
         height={120}
         className="rounded-full"
       />
       <div className="flex flex-col justify-between items-end">
-        <h2 className="text-xl font-bold">{name}</h2>
-        {balance < 0 ? (
+        <h2 className="text-xl font-bold">{contact.name}</h2>
+        {contact.balance < 0 ? (
           <p className="py-5 font-bold text-[#ce0f0f]">
-            You owe {name} {-balance} €
+            You owe {contact.name} {-contact.balance} €
           </p>
-        ) : balance === 0 ? (
-          <p className="py-5">You and {name} are even</p>
+        ) : contact.balance === 0 ? (
+          <p className="py-5">You and {contact.name} are even</p>
         ) : (
           <p className="py-5 font-bold text-[#199561]">
-            {name} owes you {balance} €
+            {contact.name} owes you {contact.balance} €
           </p>
         )}
 
         <button
           className="w-[72px] h-[41px] bg-[#f8a23d] hover:bg-[#ffc64a] font-bold text-white rounded-[10px] shadow-md"
-          onClick={onSelect}
+          onClick={handleOnSelect}
         >
           Select
         </button>
